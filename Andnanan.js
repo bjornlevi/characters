9ad9e6f9b2d8488d26e288822888bba7b764a854
bjornlevi <bjornlevi@gmail.com>
2015-11-29 10:22:36 +0000

    first character

---
+++ Andnanan.js
@@ -0,0 +1,148 @@
+var charJson =
+{
+    "name": "Andnanan",
+    "classes": {
+        "Rogue": {
+            "level": 9,
+            "hitdice": "d8"
+        }
+    },
+    "inspirationpoints": 0,
+    "hitpoints": 66,
+    "ac": 16,
+    "speed": 30,
+    "mainattributes": {
+        "background": "Spy",
+        "race": "High (moon) elf",
+        "alignment": "Chaotic Good",
+        "playername": "Björn Leví",
+        "xp": 34000
+    },
+    "appearance": {
+        "age": 27,
+        "height": "5 feet 3",
+        "weight": "117 lbs",
+        "eyes": "brown",
+        "skin": "pale",
+        "hair": "brown"
+    },
+    "abilityscores": {
+        "str": 12,
+        "dex": 18,
+        "con": 14,
+        "int": 14,
+        "wis": 14,
+        "cha": 14
+    },
+    "savingthrow_proficiencies": [
+        "int", "dex"
+    ],
+    "skill_proficiences": [
+    	"acrobatics",
+    	"athletics",
+    	"athletics",
+        "deception",
+        "investigation",
+        "perception",
+        "sleight of hand",
+        "stealth"
+    ],
+    "attacks": {
+        "shortbow": {
+            "ability": "dex",
+            "extraAttackMod": 0,
+            "extraDmgMod": 0,
+            "die": "1d6",
+            "proficient": true,
+            "type": "piercing"
+        },
+        "scimitar": {
+            "ability": "dex",
+            "die": "1d6",
+            "proficient": true,
+            "type": "slashing"
+        },
+        "blowgun": {
+            "ability": "dex",
+            "die": "1",
+            "proficient": true,
+            "type": "piercing"
+        }
+    },
+    "equipment": {
+        "coins": {
+            "copper": 0,
+            "silver": 0,
+            "electrum": 0,
+            "gold": 0,
+            "platinum": 0
+        },
+        "other": [
+            "backpack",
+            "thieves tools",
+            "burglars pack",
+            "dungeoneers pack",
+            "40 blowgun darts",
+            "20 arrows",
+            "leather armor",
+            "2 daggers",
+            "scimitar",
+            "blowgun",
+            "short bow",
+            "antitoxin",
+            "lock",
+            "hourglass"
+        ]
+    },
+    "personality": {
+        "traits": "The first thing I do in a new placeis note the locations of everything valuable - where such things could be hidden or who knows about where the valuables are",
+        "ideals": "Exposure: The evils of the world must be exposed.",
+        "bonds": "Something important was taken from me, and I aim to steal it back",
+        "flaws": "When I see something valuable, I cant think about anything but how to steal it."
+    },
+    "features": [
+        "Age: Can live to be 750 years old",
+        "Darkvision: 60 feet",
+        "Keen senses: Gain proficiency in perception",
+        "Fey ancestry: Advantage against charm and cant be magically put to sleep",
+        "Trance: Meditation for 4 hours instead of sleep",
+        "Elf weapon training: Proficient with longsword, shortsword, longbow and shortbow",
+        "Cantrip: You know one cantrip from the wizard spell list (int) - Minor Illusion",
+        "Extra language: You know one additional language",
+        "Armor: Light",
+        "Weapons: Simple, hand xbows, rapiers, scimitar, blowgun",
+        "Expertise: Double proficiency with four skills (stealth, investigation, athletics, deception",
+        "Sneak attack: 5d6",
+        "Thieves cant: Secret signs and symbols",
+        "Cunning action: Bonus action (dash, disengage, hide, Fast hands: sleight of hand, thieves tools, use an object)",
+        "Second story work: Climbing costs no extra movement, running jump +dex modifier",
+        "Uncanny dodge: (reaction) half incoming damage",
+        "Evasion: Dex saving throw = half damage / no damage",
+        "Ability score improvement: +2 dex",
+        "Expertise: Double proficiency with two skills",
+        "Supreme sneak: Advantage on stealth if 1/2 move or less",
+        "Ability score improvement: Observant; +1 int, lip reading, +5 to passive investigation and perception",
+        "Mission: Information, heirlooms",
+        "Echo of the Great Game: Years of stealth and trickery have taught you how to not draw attention to yourself. The way you walk, talk and move are deliberately forgettable, and utterly unextraordinary. While you are wearing common clothes, no one can remember your face nor any features about you, unless they actively make an effort to do so. In addition, you know how to write in a special code that can only be read by people trained in deciphering it."
+    ],
+    "spellcasting": {
+        "spellcastingAbility": "int",
+        "spellSlots": {
+            "spells0": 1
+        },
+        "spells": {
+            "spells0": ["Minor illusion"]
+        }
+    },
+    "proficiencies": [
+        "Light armor",
+        "Simple weapons, longsword, shortsword, longbow, shortbow, rapier, hand xbow, scimitar, blowgun",
+        "Disguise kit",
+        "poisoners kit"
+    ],
+    "languages": [
+        "Common",
+        "Elvish",
+        "Draconic"
+    ]
+};
